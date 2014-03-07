b8cac4fc0f9284407c5000093875298d6a0cf5df
Simon Ask Ulsnes <simon@ulsnes.dk>
2014-03-07 18:12:03 +0100

    w::redirect

--- render.cpp
+++ render.cpp
@@ -8,4 +8,11 @@ namespace w {
     r.body = std::move(text);
     return r;
   }
+
+  Response redirect(std::string new_location, HTTPStatusCode code) {
+    Response r;
+    r.code = code;
+    r.headers["Location"] = new_location;
+    return r;
+  }
 }
