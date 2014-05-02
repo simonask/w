4c070661f8a14cfefff341aea2b4a68d85793def
Simon Ask Ulsnes <simon@ulsnes.dk>
2014-05-02 11:50:38 +0200

    Updated blog example to actually return useful JSON.

--- examples/blog/blog.cpp
+++ examples/blog/blog.cpp
@@ -1,12 +1,26 @@
 #include <w>
+#include <wayward/support/json.hpp>
 #include "models.hpp"
 
 namespace app {
+  template <typename T>
+  w::Response json_response(const w::Request& req, T&& object, w::HTTPStatusCode code = w::HTTPStatusCode::OK) {
+    w::Response response;
+    // TODO: Set headers from req
+    response.body = w::as_json(std::forward<T>(object));
+    response.code = code;
+    return response;
+  }
+
   w::Response index(w::Request& req) {
     p::Context ctx;
-    auto posts = p::from<Post>(ctx).where(p::column(&Post::published_at) <= DateTime::now()).order(&Post::published_at).reverse_order();
+    auto posts = p::from<Post>(ctx)
+      .where(p::column(&Post::published_at) <= DateTime::now())
+      .inner_join(&Post::author)
+      .order(&Post::published_at)
+      .reverse_order();
     auto all = posts.all();
-    return w::not_found();
+    return json_response(req, all);
   }
 
   w::Response get_post(w::Request& req) {
