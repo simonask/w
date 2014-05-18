fa14b56dee1d394a56cf5bd24a5ffa335ed3cf37
Simon Ask Ulsnes <simon@ulsnes.dk>
2014-05-18 20:21:06 +0200

    More test cases for Fiber.

--- tests/wayward/support/fiber_test.cpp
+++ tests/wayward/support/fiber_test.cpp
@@ -10,4 +10,48 @@ namespace {
     f();
     EXPECT_EQ(true, ran);
   }
+
+  TEST(Fiber, calls_other_fiber) {
+    std::unique_ptr<Fiber> b;
+    int n = 0;
+    std::unique_ptr<Fiber> a (new Fiber { [&]() {
+      while (n < 5) {
+        b->resume();
+      }
+    }});
+    b = std::unique_ptr<Fiber>(new Fiber { [&]() {
+      while (true) {
+        n += 1;
+        a->resume();
+      }
+    }});
+
+    a->resume();
+    EXPECT_EQ(5, n);
+  }
+
+  TEST(Fiber, terminates_gracefully) {
+    Fiber& main_fiber = Fiber::current();
+    int number = 0;
+
+    Fiber f { [&]() {
+      struct Foo {
+        int& n;
+        Foo(int& n) : n(n) {}
+        ~Foo() {
+          n = 123;
+        }
+      };
+
+      Foo foo { number };
+      number = 789;
+      main_fiber.resume();
+      number = 456;
+    }};
+
+    f();
+    EXPECT_EQ(789, number);
+    f.terminate();
+    EXPECT_EQ(123, number);
+  }
 }
